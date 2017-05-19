166d259d05eb25296938615f30a2b26961e5441a
Neil Stevenson <neilstevenson@users.noreply.github.com>
2017-05-19 17:29:53 +0200

    Fix version5 so only one client started

--- version5/src/main/java/neil/demo/barcelona/MyConfig.java
+++ version5/src/main/java/neil/demo/barcelona/MyConfig.java
@@ -8,9 +8,6 @@ import org.springframework.cache.jcache.JCacheCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.client.config.XmlClientConfigBuilder;
 import com.hazelcast.core.HazelcastInstance;
 
 /**
@@ -26,16 +23,12 @@ import com.hazelcast.core.HazelcastInstance;
 @Configuration
 public class MyConfig {
 	
-	@Bean
-	public HazelcastInstance hazelcastInstance() throws Exception {
+	static {
 		/* These can't be varied, so don't put in a yml/properties file.
 		 */
 		System.setProperty("hazelcast.jcache.provider.type", "client");
 		System.setProperty("spring.cache.jcache.provider", "com.hazelcast.client.cache.impl.HazelcastClientCachingProvider");
 		System.setProperty("spring.cache.type", "jcache");
-
-		ClientConfig clientConfig = new XmlClientConfigBuilder("hazelcast-client.xml").build();
-		return HazelcastClient.newHazelcastClient(clientConfig);
 	}
 
 	/* Needs Hazelcast instance to exist first
